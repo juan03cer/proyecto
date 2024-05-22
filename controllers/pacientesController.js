91852bc430efea0b0a9ede408cc2c54864e120fc
juan03cer <161520998+juan03cer@users.noreply.github.com>
2024-05-22 09:44:15 -0600

    Add files via upload

---
+++ controllers/pacientesController.js
@@ -0,0 +1,316 @@
+import {unlink} from 'node:fs/promises'
+import { validationResult } from "express-validator"
+import { Companyseguros,Titularseguridadsocial,Seguridadsocial,Paciente ,} from '../models/index.js'
+
+const admin = async(req, res) =>{
+    const{id}= req.usuario
+
+    const paciente = await Paciente.findAll({
+        where:{
+            usuarioid:id
+        },
+        include:[
+            { model: Companyseguros , as:'companyseguro'},
+            {model: Seguridadsocial, as:'seguridadsocial' },
+            {model:Titularseguridadsocial,as:'titularseguridadsocial'}
+        ],
+
+    })
+
+
+    res.render('pacientes/admin',{
+        pagina:'Pacientes Registrados',
+        paciente,
+        csrfToken: req.csrfToken(),
+    })
+}
+
+//formulario para crear 
+const crear = async(req,res) =>{
+    //Consultar Modelo de Precio y Categorias
+    const [companysegurosid,titularseguridadsocialid,seguridadsocialid] = await Promise.all([
+        Companyseguros.findAll(),
+        Titularseguridadsocial.findAll(),
+        Seguridadsocial.findAll()
+        
+    ])
+
+   
+    res.render('pacientes/crear',{
+        pagina:'Registrar Paciente',
+        csrfToken: req.csrfToken(),
+        companysegurosid,
+        titularseguridadsocialid,
+        seguridadsocialid,
+        datos:{}
+    })
+
+}
+
+const guardar = async (req,res)=>{
+    //validacion
+    let resultado = validationResult(req)
+
+    if(!resultado.isEmpty()){
+
+        const [companysegurosid,titularseguridadsocialid,seguridadsocialid] = await Promise.all([
+            Companyseguros.findAll(),
+            Titularseguridadsocial.findAll(),
+            Seguridadsocial.findAll()
+            
+        ])
+    
+       
+      return  res.render('pacientes/crear',{
+            pagina:'Registrar Paciente',
+            csrfToken: req.csrfToken(),
+            companysegurosid,
+            titularseguridadsocialid,
+            seguridadsocialid,
+            errores:resultado.array(),
+            datos:req.body
+
+        })
+    }
+
+    //Crear un registro
+    const{nombre,datomedico,sexo,seguridadsocialid,segdgasmdcs,companysegurosid,titularseguridadsocialid,correo,numpaciente,telrecados,calle,lat,lng} =req.body
+
+
+    const{id: usuarioid} =req.usuario
+
+    try{
+        const pacienteGuardado = await Paciente.create({
+            nombre,
+            datomedico,
+            sexo,
+            seguridadsocialid,
+            segdgasmdcs,
+            companysegurosid,
+            titularseguridadsocialid,
+            correo,
+            numpaciente,
+            telrecados,
+            calle,
+            lat,
+            lng,
+            usuarioid,
+            imagen:''
+            
+        })
+
+        const {id} = pacienteGuardado
+        res.redirect(`/pacientes/agregar-imagen/${id}`)
+
+    } catch(error){
+        console.log(error)
+    }
+}
+
+const agregarImagen = async(req,res) =>{
+
+    const{id} =req.params
+
+    //Validar que el paciente exista
+    const paciente =await Paciente.findByPk(id)
+    if(!paciente){
+        return res.redirect('/mis-pacientes')
+    }
+
+    //validar que el paciente no este dado de alta
+    if(paciente.publicado){
+        return res.redirect('/mis-pacientes')
+    }
+
+
+    //validar que el paciente lo puede modificar el usuario
+    if(req.usuario.id.toString() !== paciente.usuarioid.toString()){
+        return res.redirect('/mis-pacientes')
+    }
+
+    res.render('pacientes/agregar-imagen',{
+        pagina:`Agregar Imagen del paciente : ${paciente.nombre}`,
+         csrfToken: req.csrfToken(),
+         paciente,
+       
+
+    })
+
+}
+
+const almacenarImagen = async (req,res,next) =>{
+    const{id} =req.params
+
+    //Validar que el paciente exista
+    const paciente =await Paciente.findByPk(id)
+    if(!paciente){
+        return res.redirect('/mis-pacientes')
+    }
+
+    //validar que el paciente no este dado de alta
+    if(paciente.publicado){
+        return res.redirect('/mis-pacientes')
+    }
+
+
+    //validar que el paciente lo puede modificar el usuario
+    if(req.usuario.id.toString() !== paciente.usuarioid.toString()){
+        return res.redirect('/mis-pacientes')
+    }
+
+    res.render('pacientes/agregar-imagen',{
+        pagina:`Agregar Imagen del paciente : ${paciente.nombre}`,
+         csrfToken: req.csrfToken(),
+         paciente,
+       
+
+    })
+
+    try{
+        // console.log(req.file)
+        //Almacenar Imagen y Publicar propiedad
+        paciente.imagen = req.file.filename
+
+        paciente.publicado = 1
+
+        await paciente.save()
+        
+        next()
+
+    } catch(error){
+        console.log(error)
+    }
+
+}
+
+const editar =async(req,res)=>{
+
+    const{id}=req.params
+    //Validar que el paciente exista
+    const paciente = await Paciente.findByPk(id)
+
+    if(!paciente){
+        return res.redirect('/mis-pacientes')
+    }
+
+    //Revisra que quien visita la url ,es quien creo al paciente
+    if(paciente.usuarioid.toString() !== req.usuario.id.toString()){
+        return res.redirect('/mis-pacientes')
+    }
+    //Consultar Modelo de Precio y Categorias
+    const [companysegurosid,titularseguridadsocialid,seguridadsocialid] = await Promise.all([
+        Companyseguros.findAll(),
+        Titularseguridadsocial.findAll(),
+        Seguridadsocial.findAll()
+        
+    ])
+
+    res.render('pacientes/editar',{
+        pagina:`Editar Paciente: ${paciente.nombre}`,
+        csrfToken: req.csrfToken(),
+        companysegurosid,
+        titularseguridadsocialid,
+        seguridadsocialid,
+        datos:paciente
+    })
+}
+
+const guardarCambios= async(req,res)=>{
+
+    //verificar la validacion
+    let resultado = validationResult(req)
+
+    if(!resultado.isEmpty()){
+
+        const [companysegurosid,titularseguridadsocialid,seguridadsocialid] = await Promise.all([
+            Companyseguros.findAll(),
+            Titularseguridadsocial.findAll(),
+            Seguridadsocial.findAll()
+            
+        ])
+    
+       
+        res.render('pacientes/editar',{
+            pagina:'Editar Paciente',
+            csrfToken: req.csrfToken(),
+            companysegurosid,
+            titularseguridadsocialid,
+            seguridadsocialid,
+            errores:resultado.array(),
+            datos:req.body
+        })
+    }
+
+    const{id}=req.params
+    //Validar que el paciente exista
+    const paciente = await Paciente.findByPk(id)
+
+    if(!paciente){
+        return res.redirect('/mis-pacientes')
+    }
+
+    //Revisra que quien visita la url ,es quien creo al paciente
+    if(paciente.usuarioid.toString() !== req.usuario.id.toString()){
+        return res.redirect('/mis-pacientes')
+    }
+
+    //Reescribir el objeto y actualizarlo
+    try{
+        const{nombre,datomedico,sexo,seguridadsocialid,segdgasmdcs,companysegurosid,titularseguridadsocialid,correo,numpaciente,telrecados,calle,lat,lng} =req.body
+
+        paciente.set({
+            nombre,
+            datomedico,
+            sexo,
+            seguridadsocialid,
+            segdgasmdcs,
+            companysegurosid,
+            titularseguridadsocialid,
+            correo,
+            numpaciente,
+            telrecados,
+            calle,
+            lat,
+            lng
+
+        })
+        await paciente.save()
+        res.redirect('/mis-pacientes')
+    }catch(error){
+        console.log(error)
+    }
+}
+
+const eliminar = async (req,res) =>{
+    const{id}=req.params
+    //Validar que el paciente exista
+    const paciente = await Paciente.findByPk(id)
+
+    if(!paciente){
+        return res.redirect('/mis-pacientes')
+    }
+
+    //Revisra que quien visita la url ,es quien creo al paciente
+    if(paciente.usuarioid.toString() !== req.usuario.id.toString()){
+        return res.redirect('/mis-pacientes')
+    }
+
+    // Eliminar la imagen
+    await unlink(`public/uploads/${paciente.imagen}`)
+
+    //Eliminar paciente
+    await paciente.destroy()
+    res.redirect('/mis-pacientes')
+}
+
+export{
+    admin,
+    crear,
+    guardar,
+    agregarImagen,
+    almacenarImagen,
+    editar,
+    guardarCambios,
+    eliminar
+    
+}
