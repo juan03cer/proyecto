d86daec7a7efae1048c990ccf9df3508aa0f50de
juan03cer <161520998+juan03cer@users.noreply.github.com>
2024-05-23 19:10:03 -0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,9 @@ export default {
     mode:'development',
     entry:{
         mapa:'./src/js/mapa.js',
-        agregarImagen:'./src/js/agregarImagen.js'
+        agregarImagen:'./src/js/agregarImagen.js',
+        mostrarMapa:'./src/js/mostrarMapa.js',
+        mapaInicio:'./src/js/mapaInicio.js'
 
     },
     output:{
