83714e6b44fe819c14582ca4b77a68f4a9a1e6a1
juan03cer <161520998+juan03cer@users.noreply.github.com>
2024-05-23 19:08:26 -0600

    Add files via upload

---
+++ controllers/appController.js
@@ -0,0 +1,26 @@
+const inicio = (req, res )=>{
+
+    
+res.render('inicio',{
+    pagina:'Inicio'
+})
+}
+
+const seguridadSocial=(req,res) =>{
+
+}
+
+const noEncontrado=(req,res)=>{
+
+}
+
+const buscador=(req,res)=>{
+
+}
+
+export{
+    inicio,
+    seguridadSocial,
+    noEncontrado,
+    buscador
+}
