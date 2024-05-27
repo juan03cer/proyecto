70af285addac944c54493d443e68542345259806
juan03cer <161520998+juan03cer@users.noreply.github.com>
2024-05-27 11:22:34 -0600

    Delete routes directory

--- routes/apiRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { pacientes } from '../controllers/apiController.js'
-const router =express.Router()
-
-router.get('/pacientes',pacientes)
-
-
-export default router
